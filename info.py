8710cdd75b34150792938a02a74f1f4728b97e91
Abhishek Kumar <Nain95552@gmail.com>
2026-02-28 21:12:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,9 +37,9 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-100'))
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002520793005')) 
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-100') 
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002520793005') 
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-100') 
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002520793005') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://thammadu8_db_user:y5yzg5xktilw3Fil@cluster0.2vqa8ms.mongodb.net/?appName=Cluster0")
